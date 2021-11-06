932c7ed9dde6a2aae562068fa44b102e0ee5ff93
FocusedBat <58144607+FocusedBat@users.noreply.github.com>
2021-11-06 15:22:31 -0400

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,23 +1,24 @@
 class Vertex:
     def __init__(self, v_type):
         self.v_type = v_type
-        self.adjacent = []  # ordered by up left right down
+        self.adjacent = []  # ordered by up left down right
         self.visited = False
         self.is_obstacle = (v_type == 'X')
 
     # adds edge from self to vertice
-    def add_edge(self, vertex):
+    def add_adj(self, vertex):
         self.adjacent.append(vertex)
         if len(self.adjacent) == 4:
             self.reorder_adj()
 
     # gets adjacent from index
-    def get_adjacent(self, index):
+    def get_adj(self, index):
         return self.adjacent[index]
 
     # reorders after all adj are added, changes adj u l r d to u l d r (swap d and r)
     def reorder_adj(self):
         self.adjacent[2], self.adjacent[3] = self.adjacent[3], self.adjacent[2]
+
 
 class Graph:
     def __init__(self):
@@ -37,28 +38,26 @@ class Graph:
                 self.root = row[0]
             self.find_r_or_g(grid[y][0], row[0])
             self.connect_vertices(prev_row[0], row[0])  # adds row up and prevrow down
-            row[0].add_edge(None)  # adds left at start of row
+            row[0].add_adj(None)  # adds left at start of row
             for x in range(1, len(grid[0])):
                 row.append(Vertex(grid[y][x]))
                 self.find_r_or_g(grid[y][x], row[x])
                 self.connect_vertices(prev_row[x], row[x])  # connects current vertex with vertex above it
-                # reorder previous row (does not include initial none row)
                 self.connect_vertices(row[x-1], row[x])  # connects current vertex with previous vertex on left
-            row[-1].add_edge(None)  # adds right to end of list
+            row[-1].add_adj(None)  # adds right to end of list
             prev_row = row[:]
             row.clear()
 
         # adds none to 'down' and reorders adjacent list
         for x in range(len(prev_row)):
-            prev_row[x].add_edge(None)
-
+            prev_row[x].add_adj(None)
 
     def connect_vertices(self, vertex1, vertex2):
         # vertex1 on left/above vertice2 on right/below
         if vertex1 is not None:
-            vertex1.add_edge(vertex2)
+            vertex1.add_adj(vertex2)
         if vertex2 is not None:
-            vertex2.add_edge(vertex1)
+            vertex2.add_adj(vertex1)
 
     def find_r_or_g(self, c, vertex):
         if c == 'G':
@@ -67,13 +66,13 @@ class Graph:
             self.goal = vertex
 
     def __str__(self):
-        msg = "Grid\n"
+        msg = ""
         y_vertex = self.root
         while y_vertex is not None:
             x_vertex = y_vertex
             while x_vertex is not None:
                 msg += f"{x_vertex.v_type}"
-                x_vertex = x_vertex.get_adjacent(3)
+                x_vertex = x_vertex.get_adj(3)
             msg += '\n'
-            y_vertex = y_vertex.get_adjacent(2)
+            y_vertex = y_vertex.get_adj(2)
         return msg
